5d3da7b11623b7b80512f6ae16b1ce3816a9ebbf
Daniel <itzdanielmichael@gmail.com>
2022-09-06 19:30:19 -0400

    fix apiConfig

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: 'https://2132-100-33-108-135.ngrok.io/api',
+  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: 'https://2132-100-33-108-135.ngrok.io/api',
 });
 
 export default api;
